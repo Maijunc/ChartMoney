e9cddda68e4b4c03c4d6a64884811c4f521c45aa
Maijunc <191229223@qq.com>
2026-01-06 17:41:06 +0800

    feat: init project structure (FastAPI + Vue3)

--- finance-project/backend/database.py
+++ finance-project/backend/database.py
@@ -4,7 +4,13 @@ from sqlalchemy.orm import sessionmaker
 
 # ⚠️ 注意修改这里的密码！
 # 格式: mysql+pymysql://用户名:密码@IP:端口/数据库名
-SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:123456@localhost:3306/finance_db"
+# SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:123456@localhost:3306/finance_db"
+
+# 修改前
+# ...@localhost:3306/...
+
+# 修改后 (强制指定 IPv4)
+SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:123456@127.0.0.1:3306/finance_db"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
