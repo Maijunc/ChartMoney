81b2cf3531c654ba3cee2c83f5270e98b1274ef9
wtm2648 <56440072+wtm2648@users.noreply.github.com>
2026-01-08 15:52:29 +0800

    添加了账单模型类
    添加了一个账单模型类

--- finance-project/backend/models.py
+++ finance-project/backend/models.py
@@ -1,5 +1,6 @@
-from sqlalchemy import func, Column, String, BigInteger, DateTime, ForeignKey, Boolean
+from sqlalchemy import func, Column, String, BigInteger, DateTime, ForeignKey, Boolean, DECIMAL
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
+from sqlalchemy.dialects.mysql import TINYINT   # TINYINT是MySql数据库特有类型，属于方言
 from typing import List
 from datetime import datetime
 
@@ -32,6 +33,7 @@ class User(Base):   # 用户表的模型类
     )
 
     bill_categories: Mapped[List["Bill_Category"]] = relationship(back_populates="user")    # 一个用户对应多个分类，使用List
+    bills: Mapped[List["Bill"]] = relationship(back_populates="user")   # 一个用户对应多个账单
 
 
 class Bill_Category(Base):
@@ -43,3 +45,34 @@ class Bill_Category(Base):
     name: Mapped[str] = mapped_column(type_=String(20), comment="分类名称", nullable=False)
 
     user: Mapped["User"] = relationship(back_populates="bill_category", uselist=False)  # 一个账单分类对应一个用户
+    bills: Mapped["Bill"] = relationship(back_populates="bill_category")    # 一个账单分类对应多个账单
+
+
+class Bill(Base):
+    __tablename__ = "bill"
+
+    id: Mapped[int] = mapped_column(type_=BigInteger ,primary_key=True, comment="账单id", autoincrement=True)
+    user_id: Mapped[int] = mapped_column(BigInteger, ForeignKey("user.id"))
+    category_id: Mapped[int] = mapped_column(BigInteger, ForeignKey("bill_category.id"))
+    amount: Mapped[float] = mapped_column(DECIMAL(10, 2), nullable=False, comment="金额")
+    type: Mapped[int] = mapped_column(TINYINT, nullable=False, comment="1代表收入，2代表支出")
+    remark: Mapped[str] = mapped_column(String(255), nullable=True, comment="账单备注")
+    bill_time: Mapped[datetime] = mapped_column(DateTime, nullable=False, comment="订单发生时间")
+    create_time: Mapped[datetime] = mapped_column(
+        type_=DateTime,
+        default=func.now(),
+        server_default=func.now(),
+        comment="创建时间"
+    )
+    update_time: Mapped[datetime] = mapped_column(
+        type_=DateTime,
+        default=func.now(),
+        server_default=func.now(),
+        onupdate=func.now(),
+        comment="更新时间"
+    )
+
+    user: Mapped["User"] = relationship(back_populates="bill", uselist=False)   # 一个账单对应一个用户
+    bill_category: Mapped["Bill_Category"] = relationship(back_populates="bill", uselist=False)    # 一个账单对应一个分类
+
+
